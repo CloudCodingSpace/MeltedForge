d9475cf9c5c9653a057eddf4651f1820fee70230
CloudCodingSpace <subhamaygamer2127@outlook.com>
2025-04-14 22:28:23 +0530

    Selected the suitable GPU for usage

--- MeltedForge/src/mf/renderer/vk/ctx.c
+++ MeltedForge/src/mf/renderer/vk/ctx.c
@@ -2,6 +2,66 @@
 
 #include <GLFW/glfw3.h>
 
+#include <string.h>
+
+static MFVkBackendQueueData GetDeviceQueueData(VkSurfaceKHR surface, VkPhysicalDevice device) {
+    MFVkBackendQueueData data = {-1};
+
+    u32 count = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &count, mfnull);
+    VkQueueFamilyProperties props[count];
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &count, props);
+
+    for(u32 i = 0; i < count; i++) {
+        if(props[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            data.gQueueIdx = i;
+        if(props[i].queueFlags & VK_QUEUE_TRANSFER_BIT)
+            data.tQueueIdx = i;
+        if(props[i].queueFlags & VK_QUEUE_COMPUTE_BIT)
+            data.cQueueIdx = i;
+            
+        VkBool32 presentSupport = VK_FALSE;
+        vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+
+        if(presentSupport)
+                data.pQueueIdx = i;
+    }
+
+    return data;
+}
+
+static bool IsQueueDataComplete(MFVkBackendQueueData data) {
+    return data.cQueueIdx != -1 && data.gQueueIdx != -1 && data.pQueueIdx != -1 && data.tQueueIdx != -1;
+}
+
+static bool IsDeviceUsable(VkSurfaceKHR surface, VkPhysicalDevice device) {
+    MFVkBackendQueueData data = GetDeviceQueueData(surface, device);
+
+    bool extSupport = false;
+    {
+        const char* deviceExts[] = {
+            VK_KHR_SWAPCHAIN_EXTENSION_NAME
+        };
+        u32 deviceExtCount = 1;
+
+        u32 count = 0;
+        vkEnumerateDeviceExtensionProperties(device, mfnull, &count, mfnull);
+        VkExtensionProperties props[count];
+        vkEnumerateDeviceExtensionProperties(device, mfnull, &count, props);
+
+        for(u32 i = 0; i < count; i++) {
+            for(u32 j = 0; j < deviceExtCount; j++) {
+                if(strcmp(props[i].extensionName, deviceExts[j]) == 0) {
+                    extSupport = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    return IsQueueDataComplete(data) && extSupport;
+}
+
 void mfVkBckndCtxInit(MFVkBackendCtx* ctx, const char* appName, MFWindow* window) {
     ctx->callbacks = mfnull; // TODO: Create a custom allocator
 
@@ -31,6 +91,25 @@ void mfVkBckndCtxInit(MFVkBackendCtx* ctx, const char* appName, MFWindow* window
     // Surface
     {
         VK_CHECK(glfwCreateWindowSurface(ctx->instance, mfGetWindowHandle(window), ctx->callbacks, &ctx->surface));
+    }
+    // Physical Device 
+    {
+        u32 deviceCount = 0;
+        VK_CHECK(vkEnumeratePhysicalDevices(ctx->instance, &deviceCount, mfnull));
+        VkPhysicalDevice devices[deviceCount];
+        VK_CHECK(vkEnumeratePhysicalDevices(ctx->instance, &deviceCount, devices));
+
+        // TODO: Select the most capable physical device if there are more than one
+        for(int i = 0; i < deviceCount; i++) {
+            if(IsDeviceUsable(ctx->surface, devices[i])) {
+                ctx->physicalDevice = devices[i];
+                break;
+            }
+        }
+
+        MF_ASSERT(ctx->physicalDevice == mfnull, mfGetLogger(), "(From the vulkan backend) Failed to select a suitable GPU in the current PC!");
+
+        ctx->qData = GetDeviceQueueData(ctx->surface, ctx->physicalDevice);
     }
 }
 
--- MeltedForge/src/mf/renderer/vk/ctx.h
+++ MeltedForge/src/mf/renderer/vk/ctx.h
@@ -4,11 +4,19 @@
 
 #include "window/mfwindow.h"
 
+typedef struct MFVkBackendQueueData_s {
+    i32 gQueueIdx, tQueueIdx, pQueueIdx, cQueueIdx;
+    VkQueue gQueue, tQueue, pQueue, cQueue;
+} MFVkBackendQueueData;
+
 typedef struct MFVkBackendCtx_s {
     VkAllocationCallbacks* callbacks;
     VkInstance instance;
     VkSurfaceKHR surface;
+
+    MFVkBackendQueueData qData;
     VkPhysicalDevice physicalDevice;
+    VkDevice device;
 } MFVkBackendCtx;
 
 void mfVkBckndCtxInit(MFVkBackendCtx* ctx, const char* appName, MFWindow* window);
