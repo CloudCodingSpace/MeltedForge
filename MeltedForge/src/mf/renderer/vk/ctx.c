565aa78fce0e59cace4703acc053ce893d0f7318
CloudCodingSpace <subhamaygamer2127@outlook.com>
2025-05-17 16:50:41 +0530

    Removed nucklear from the project

--- MeltedForge/src/mf/renderer/vk/ctx.c
+++ MeltedForge/src/mf/renderer/vk/ctx.c
@@ -6,17 +6,6 @@
 
 #include "common.h"
 
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_STANDARD_IO
-#define NK_INCLUDE_STANDARD_VARARGS
-#define NK_INCLUDE_DEFAULT_ALLOCATOR
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
-#define NK_INCLUDE_COMMAND_USERDATA
-#include <nuklear/nuklear.h>
-#include <nuklear/nuklear_glfw_vulkan.h>
-
 static VulkanBackendQueueData GetDeviceQueueData(VkSurfaceKHR surface, VkPhysicalDevice device) {
     VulkanBackendQueueData data = {-1};
 
