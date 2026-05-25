10521927205e35bf3e25460a13854f9ccc99d43b
CloudCodingSpace <subhamaygamer2127@outlook.com>
2026-05-25 18:17:36 +0530

    Added some doxygen comments to MFTimer

--- MeltedForge/src/mf/core/mftime.h
+++ MeltedForge/src/mf/core/mftime.h
@@ -13,16 +13,37 @@ typedef struct MFTimer_s {
     bool started;
 } MFTimer;
 
+// @brief Starts the timer
+// @param timer A valid non-null pointer to a `MFTimer`
 void mfTimerStart(MFTimer* timer);
+
+// @brief Stops the timer
+// @param timer A valid non-null pointer to a `MFTimer`
 void mfTimerEnd(MFTimer* timer);
+
+// @brief Resets the timer
+// @param timer A valid non-null pointer to a `MFTimer`
 void mfTimerReset(MFTimer* timer);
 
+// @brief Gets the elapsed time since the engine was initialised
 f64 mfGetTimeElapsed(void);
+
+// @brief Gets current second
 f64 mfGetCurrentTimeSecs(void);
+
+// @brief Gets the current time in minutes
 u64 mfGetCurrentTimeMins(void);
+
+// @brief Gets the current hour
 u64 mfGetCurrentTimeHours(void);
+
+// @brief Gets the current day
 u64 mfGetCurrentTimeDays(void);
+
+// @brief Gets the current month
 u32 mfGetCurrentTimeMonths(void);
+
+// @brief Gets the current year
 u64 mfGetCurrentTimeYears(void);
 
 #ifdef __cplusplus
