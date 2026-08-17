61b0cbf061cae8d48c6242015612471423562ebe
CloudCodingSpace <subhamaygamer2127@outlook.com>
2026-08-17 19:48:08 +0530

    Starting the rendergraph :)
    Just wrote the initial layout and description interfaces etc

---
+++ MeltedForge/src/mf/renderer/mfrendergraph.c
@@ -0,0 +1,14 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mfrendergraph.h"
+
+struct MFRenderGraph_s {
+    MFRenderGraphConfig config;
+    // TODO: complete these
+};
+
+#ifdef __cplusplus
+}
+#endif
---
+++ MeltedForge/src/mf/renderer/mfrendergraph.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "core/mfutils.h"
+#include "core/mfcore.h"
+
+#include "mfrenderer.h"
+#include "mfutil_types.h"
+
+typedef enum MFRenderGraphAttachmentType_e {
+    MF_RENDER_GRAPH_ATTACHMENT_TYPE_COLOR_ATTACHMENT = 1 << 0,
+    MF_RENDER_GRAPH_ATTACHMENT_TYPE_INPUT_ATTACHMENT = 1 << 1,
+    MF_RENDER_GRAPH_ATTACHMENT_TYPE_DEPTH_STENCIL_ATTACHMENT = 1 << 2,
+    MF_RENDER_GRAPH_ATTACHMENT_TYPE_COUNT = 3,
+} MFRenderGraphAttachmentType;
+
+typedef struct MFRenderGraphAttachmentDesc_s {
+    MFRenderGraphAttachmentType type;
+    MFFormat format;
+    u32 widh, height;
+    MFVec4 clearColor;
+} MFRenderGraphAttachmentDesc;
+
+typedef struct MFRenderGraphPassDesc_s {
+    const char* name;
+    u32* inputAttachments;
+    u32* depthStencilAttachment;
+    u32* outputColorAttachmentCount;
+    u32 inputAttachmentCount;
+    u32 outputColorAttachmentCount;
+
+    void* userData;
+    void (*passDrawCallback)(void* userData);
+} MFRenderGraphPassDesc;
+
+typedef struct MFRenderGraphConfig_s {
+    u32 attachmentCount;
+    u32 passCount;
+    MFRenderGraphAttachmentDesc* attachments;
+    MFRenderGraphPassDesc* passes;
+} MFRenderGraphConfig;
+
+typedef struct MFRenderGraph_s MFRenderGraph;
+
+// TODO: Write the functions next
+
+#ifdef __cplusplus
+}
+#endif
